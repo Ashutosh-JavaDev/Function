bfb1b88bc70a3ce35f5e27589b83b5c07f299e72
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-18 22:53:48 +0530

    Project

--- NoParameterNoReturn/CountVowel.java
+++ NoParameterNoReturn/CountVowel.java
@@ -21,5 +21,8 @@ public class CountVowel {
             System.out.println("No Vowel");
         }
     }
-    
+    public static void main(String[] args) {
+        CountVowel ob=new CountVowel();
+        ob.vowel();
+    }
 }
