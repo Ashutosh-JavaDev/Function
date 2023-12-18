50d3642096618f661ae45baf3ffd1706fcbf7011
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-18 21:51:21 +0530

    Project

---
+++ NoParameterNoReturn/PerfectOrNot.java
@@ -0,0 +1,22 @@
+package Function.NoParameterNoReturn;
+
+import java.util.*;
+
+public class PerfectOrNot {
+    public void check() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int number = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (number == sum) {
+            System.out.println("Number is perfect");
+        } else {
+            System.out.println("Number is not Perfect");
+        }
+    }
+}
