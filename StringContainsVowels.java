939336f9c1e2ff5c6d058f3591689c3971171180
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-12 09:37:34 +0530

    Java Program

--- StringContainsVowels.java
+++ StringContainsVowels.java
@@ -3,7 +3,7 @@ public class StringContainsVowels {
 
 	public static void main(String[] args) {
 		System.out.println(stringContainsVowels("Hello")); // true
-		System.out.println(stringContainsVowels("TV")); // false
+		System.out.println(stringContainsVowels("ToV")); // false
 	}
 
 	public static boolean stringContainsVowels(String input) {
