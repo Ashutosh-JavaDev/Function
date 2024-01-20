3fe9ebd2fdbdc45a01d7f72a91a0d188b79ff15f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 16:53:54 +0530

    Recursion

--- ParameterAndReturn/bubbleSort.java
+++ ParameterAndReturn/bubbleSort.java
@@ -1,7 +1,24 @@
 package Function.ParameterAndReturn;
 class bubSorting{
-    void sortingBubble(int arr[],int size){
-        
+    void sortingBubble(int arr[],int i,int j,int size){
+        if(size==1){
+            return;
+        }
+        else{
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+                sortingBubble(arr, i, j+1, size-1);
+            }
+            else{
+                int temp=arr[i+1];
+                arr[i+1]=arr[i];
+                arr[i]=temp;
+                sortingBubble(arr, i, j+1, size-1);
+            }
+        }
+        System.out.print(arr[i]+" ");
     }
 }
 public class bubbleSort {
