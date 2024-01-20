c40c125d0e02c751095b73c9a9ad3edc75db18c2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-21 00:27:51 +0530

    Recursion

--- ParameterAndReturn/bubbleSort.java
+++ ParameterAndReturn/bubbleSort.java
@@ -1,30 +1,35 @@
 package Function.ParameterAndReturn;
-class bubSorting{
-    void sortingBubble(int arr[],int i,int j,int size){
-        if(size==1){
+
+class bubSorting {
+    void sortingBubble(int arr[], int i, int j, int size) {
+        if (size == 1) {
             return;
-        }
-        else{
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-                sortingBubble(arr, i, j+1, size-1);
+        } else if (i < size && j < size) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                sortingBubble(arr, i, j + 1, size - 1);
+                i++;
+                System.out.print(arr[i] + " ");
+
+            } else {
+                int temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+                sortingBubble(arr, i, j + 1, size - 1);
+                i++;
+                System.out.print(arr[i] + " ");
+
             }
-            else{
-                int temp=arr[i+1];
-                arr[i+1]=arr[i];
-                arr[i]=temp;
-                sortingBubble(arr, i, j+1, size-1);
-            }
         }
-        System.out.print(arr[i]+" ");
     }
 }
+
 public class bubbleSort {
     public static void main(String[] args) {
-        bubSorting ob=new bubSorting();
-        int arr[]={2,4,1,5,3};
-        ob.sortingBubble(arr, 0, 0+1, arr.length-1);
+        bubSorting ob = new bubSorting();
+        int arr[] = { 2, 4, 1, 5, 3 };
+        ob.sortingBubble(arr, 0, 0 + 1, arr.length - 1);
     }
 }
