aeb056178312e14202645bd572614bc0f796e8ea
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 16:33:51 +0530

    Recursion

---
+++ ParameterAndReturn/sumOfDigit.java
@@ -0,0 +1,14 @@
+package Function.ParameterAndReturn;
+class Digitsum{
+    int sumDig(int number){
+        if(number<1){
+            return 1;
+        }
+        else{
+            return (number%10)+(number/10);
+        }
+    }
+}
+public class sumOfDigit {
+    
+}
