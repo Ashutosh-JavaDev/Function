1ab2e9eb52410750f90a9b4a9cf814c02d66c03d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 15:04:59 +0530

    Reacursion

--- ParameterAndReturn/fibonacci.java
+++ ParameterAndReturn/fibonacci.java
@@ -1,20 +1,20 @@
 package Function.ParameterAndReturn;
 class fiboo{
-    void fibo(int term, int a,int b){
-        if(term<=10)
-        {
-            int c=a+b;
-              System.out.print(c+" ");
-              a=b;
-              b=c;
-            fibo(term+1, a ,b);
-        }
+    int fibo(int term, int a,int b){
+       if(term<1){
+        return 1;
+       }
+       else{
+        System.out.print(a+" ");
+        int c=a+b;
+        return fibo(term-1, b, c);
+       }
         
     }
 }
 public class fibonacci {
     public static void main(String[] args) {
         fiboo ob=new fiboo();
-        ob.fibo(1, -1,1);
+        ob.fibo(5, 0, 1);
     }
 }
