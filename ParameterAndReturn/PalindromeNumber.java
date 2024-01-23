787e3b4229a2a013a5f203022b117fce342ec0dd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-23 13:20:02 +0530

    Recursion

--- ParameterAndReturn/PalindromeNumber.java
+++ ParameterAndReturn/PalindromeNumber.java
@@ -1,7 +1,16 @@
 package Function.ParameterAndReturn;
 import java.util.Scanner;
 class Palindrome{
-    
+    void disp(int number,int rev){
+        if(number<0){
+            return;
+        }
+        else{
+            int num=number;
+            int d=num%10;
+            
+        }
+    }
 }
 public class PalindromeNumber {
     
