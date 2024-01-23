54b512b80c1aa91c894f081c4de9a26f12153a8e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-23 14:56:43 +0530

    Recursion

--- ParameterAndReturn/PalindromeNumber.java
+++ ParameterAndReturn/PalindromeNumber.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 
 class Palindrome {
     int disp(int number, int rev) {
-        if (number < 0) {
-            return -1;
+        if (number <=0) {
+            return rev;
         } else {
-            int num = number;
-            int d = num % 10;
+            int d = number % 10;
             rev = rev * 10 + d;
-            return disp(number / 10, rev);
+            return disp(number / 10,rev);
         }
     }
 }
 
 public class PalindromeNumber {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int number=sc.nextInt();
         Palindrome ob = new Palindrome();
-        ob.disp(121, 0);
+       int result= ob.disp(number, 0);
+       if(result==number){
+        System.out.println("Number is Palindrome");
+       }
+       else{
+        System.out.println("Number Is not Palindrome");
+
+       }
     }
 }
