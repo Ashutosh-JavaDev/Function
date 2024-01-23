b9528ee666d5e2b3972fbb95aa6e7e450af4ccba
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-23 11:19:06 +0530

    Recursion Program

--- ParameterAndReturn/target.java
+++ ParameterAndReturn/target.java
@@ -2,8 +2,16 @@ package Function.ParameterAndReturn;
 
 import java.util.Scanner;
 class TargetNumber{
-    void disp(int arr[],int size,int target){
-        
+    int disp(int arr[],int size,int target){
+        if(size<0){
+            return 0;
+        }
+        else if(arr[size]==target){
+            return size;
+        }
+        else{
+            return disp(arr, size-1, target);
+        }
     }
 }
 public class target {
