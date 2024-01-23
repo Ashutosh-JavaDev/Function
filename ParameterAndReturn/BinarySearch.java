640a64eed97af938ef6206c62d29d695cf6c9e2c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-23 11:48:53 +0530

    resursion

--- ParameterAndReturn/BinarySearch.java
+++ ParameterAndReturn/BinarySearch.java
@@ -1,7 +1,23 @@
 package Function.ParameterAndReturn;
 import java.util.Scanner;
 class Binsearch{
-    
+    int biSearch(int arr[],int size,int target,int s,int e){
+        if(s>e){
+            return -1;
+        }
+        else{
+            int mid=s+(e-s)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                return biSearch(arr, size, target, mid+1, e);
+            }
+            else{
+                return biSearch(arr, size, target, s, mid-1);
+            }
+        }
+    }
 }
 public class BinarySearch {
     
