9c50c185f1ebf52d047b961c4a20d68b13009770
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-11 15:31:51 +0530

    Merge Sort

--- ParameterandNoReturn/MergeSort.java
+++ ParameterandNoReturn/MergeSort.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 class merge {
     public static void mergeSort(int arr[], int size, int first, int last) {
-        int k;
+        int mid = first + (last - first) / 2;
+        int k[] = new int[first + last];
     }
 }
 
