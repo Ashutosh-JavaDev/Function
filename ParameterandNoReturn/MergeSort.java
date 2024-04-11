6450ada8cd939f2b672d761f39aef5576884b243
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-11 15:38:17 +0530

    Merge Sort

--- ParameterandNoReturn/MergeSort.java
+++ ParameterandNoReturn/MergeSort.java
@@ -6,6 +6,21 @@ class merge {
     public static void mergeSort(int arr[], int size, int first, int last) {
         int mid = first + (last - first) / 2;
         int k[] = new int[first + last];
+        int s=0;
+        int  i=first;
+        int j=mid+1;
+        while(i<=mid && j<=last){
+            if(arr[i]<=arr[j]){
+                k[s]=arr[i];
+                s++;
+                i++;
+            }
+            else{
+                k[s]=arr[j];
+                s++;
+                j++;
+            }
+        }
     }
 }
 
